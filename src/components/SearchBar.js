fb1447cacc44ff49d7e5e35cb355354ff8508cbd
hamza-ali-99 <HamzaHere>
2023-09-08 15:32:09 +0500

    Fixed Search Bar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import { Select, DatePicker, TimePicker, Input, Button, Row, Col } from 'antd';
-import moment from 'moment';
-import { connect } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { filterCars } from '../store/carReducer';
-
+import React, { useState } from "react";
+import { Select, DatePicker, TimePicker, Input, Button, Row, Col } from "antd";
+import moment from "moment";
+import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
+import { filterCars } from "../store/carReducer";
 // import 'antd/dist/antd.css';
 
 const { Option } = Select;
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-  const [searchInput, setSearchInput] = useState('');
-  const [fromTimestamp, setFromTimestamp] = useState('');
-  const [toTimestamp, setToTimestamp] = useState('');
-  const [locationInput, setLocationInput] = useState('');
-const [modelInput, setModelInput] = useState('');
-const [priceInput, setPriceInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
+  const [fromTimestamp, setFromTimestamp] = useState("");
+  const [toTimestamp, setToTimestamp] = useState("");
+  const [locationInput, setLocationInput] = useState("");
+  const [modelInput, setModelInput] = useState("");
+  const [priceInput, setPriceInput] = useState("");
 
   const handleInputChange = (event) => {
     setSearchInput(event.target.value);
@@ -32,48 +31,48 @@ const [priceInput, setPriceInput] = useState('');
       to: { timestampValue: toTimestamp },
     };
 
-    console.log("searchData==>",searchData);
+    console.log("searchData==>", searchData);
     dispatch(filterCars(searchData));
-   // dispatch(filterCars(searchInput));
+    // dispatch(filterCars(searchInput));
   };
 
   const handleLocationInputChange = (value) => {
     setLocationInput(value);
   };
-  
+
   const handleModelInputChange = (event) => {
     setModelInput(event.target.value);
   };
-  
+
   const handlePriceInputChange = (event) => {
     setPriceInput(event.target.value);
   };
-  
 
   const handleFromTimestampChange = (value) => {
     if (value) {
       const formattedTimestamp = value.toISOString();
       setFromTimestamp(formattedTimestamp);
     } else {
-      setFromTimestamp('');
+      setFromTimestamp("");
     }
   };
-  
+
   const handleToTimestampChange = (value) => {
     if (value) {
       const formattedTimestamp = value.toISOString();
       setToTimestamp(formattedTimestamp);
     } else {
-      setToTimestamp('');
+      setToTimestamp("");
     }
   };
   return (
-    <div className="container-fluid bg-white pt-3 px-lg-5">
+    <div className={`container-fluid bg-white pt-3 px-lg-5`}>
       <Row gutter={[16, 16]}>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <Select
             className="custom-select"
-            style={{ height: '50px', width: '100%' }}
+            style={{ width: "100%" }}
+            size="large"
             placeholder="Pickup Location"
             onChange={handleLocationInputChange}
           >
@@ -86,84 +85,64 @@ const [priceInput, setPriceInput] = useState('');
             <Option value="San Francisco">San Francisco</Option>
             <Option value="Houston">Houston</Option>
             <Option value="Kansas City">Kansas City</Option>
-            <Option value="Ottawa">Ottawa</Option> 
+            <Option value="Ottawa">Ottawa</Option>
           </Select>
         </Col>
-        {/* <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-          <Select
-            className="custom-select"
-            style={{ height: '50px', width: '100%' }}
-            placeholder="Drop Location"
-          >
-            <Option value="1">Location 1</Option>
-            <Option value="2">Location 2</Option>
-            <Option value="3">Location 3</Option>
-          </Select>
-        </Col> */}
-        {/* <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+        <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <DatePicker
             className="custom-select"
-            style={{ height: '50px', width: '100%' }}
-            placeholder="Pickup Date"
+            size="large"
+            style={{ width: "100%" }}
+            placeholder="From"
+            onChange={handleFromTimestampChange}
+            value={fromTimestamp ? moment(fromTimestamp) : null}
           />
-        </Col> */}
-        {/* <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-          <TimePicker
-            className="custom-select"
-            style={{ height: '50px', width: '100%' }}
-            placeholder="Pickup Time"
-          />
-        </Col> */}
+        </Col>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-  <DatePicker
-    className="custom-select"
-    style={{ height: '50px', width: '100%' }}
-    placeholder="From"
-    onChange={handleFromTimestampChange}
-    value={fromTimestamp ? moment(fromTimestamp) : null}
-  />
-</Col>
-<Col xs={24} sm={12} md={8} lg={6} xl={4}>
-  <DatePicker
-    className="custom-select"
-    style={{ height: '50px', width: '100%' }}
-    placeholder="To"
-    onChange={handleToTimestampChange}
-    value={toTimestamp ? moment(toTimestamp) : null}
-  />
-</Col>
+          <DatePicker
+            className="custom-select"
+            size="large"
+            style={{ width: "100%" }}
+            placeholder="To"
+            onChange={handleToTimestampChange}
+            value={toTimestamp ? moment(toTimestamp) : null}
+          />
+        </Col>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <Input
-          value={searchInput}
-          onChange={handleInputChange}
+            value={searchInput}
+            onChange={handleInputChange}
             className="custom-select"
-            style={{ height: '50px', width: '100%' }}
+            size="large"
+            style={{ width: "100%" }}
             placeholder="Car Name"
           />
         </Col>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <Input
-          value={modelInput}
-          onChange={handleModelInputChange}
+            value={modelInput}
+            onChange={handleModelInputChange}
             className="custom-select"
-            style={{ height: '50px', width: '100%' }}
+            size="large"
+            style={{ width: "100%" }}
             placeholder="Model"
           />
         </Col>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <Input
-           value={priceInput}
-           onChange={handlePriceInputChange}
+            value={priceInput}
+            onChange={handlePriceInputChange}
             className="custom-select"
-            style={{ height: '50px', width: '100%' }}
+            size="large"
+            style={{ width: "100%" }}
             placeholder="Price"
           />
         </Col>
         <Col xs={24} sm={12} md={8} lg={6} xl={4}>
           <Button
-          onClick={handleSearchClick}
+            onClick={handleSearchClick}
             className="btn btn-primary btn-block"
-            style={{ height: '50px', width: '100%' }}
+            style={{ height: "50px", width: "100%" }}
             type="primary"
           >
             Search
